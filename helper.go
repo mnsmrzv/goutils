0e789bacc176d962cf490f6c09b50b2489b99061
MunisMirzoev <mrzvmns@gmail.com>
2020-01-26 09:40:19 +0500

    feat: Help function added

---
+++ helper.go
@@ -0,0 +1,7 @@
+package goutils
+
+import "golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
+//unexported
+func help()  {
+	fmt.Println("Nobody helps you")
+}
---
+++ utils.go
@@ -0,0 +1,5 @@
+package goutils
+
+func Help() {
+	help()
+}
